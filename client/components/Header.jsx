23731a6ecc7a8f5e2e89b80da6182118b1a40e93
vitalityscreen <tyron.ball@gmail.com>
2025-05-23 16:53:02 +1200

    Create Header.jsx

---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+// Header.jsx
+function Header() {
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="VitalityScreen Logo" className="logo" />
+      <h1 className="title">VitalityScreen Health Assessment</h1>
+    </header>
+  );
+}
+
+export default Header;
