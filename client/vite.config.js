306a5474626a169c2eda536adfb94ff4b657477a
vitalityscreen <tyron.ball@gmail.com>
2025-05-23 14:59:54 +1200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['vitalityscreenapp1-1-front-end.onrender.com']
+  }
 })
