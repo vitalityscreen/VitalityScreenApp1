9391926342d8d23ddbafc1f0557be85fc9289f86
vitalityscreen <tyron.ball@gmail.com>
2025-05-23 00:33:08 +0000

    Add client and server directories

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('VitalityScreen backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
